14094735281288db5d9294d4c4953045c87516f9
THEOPHILE PICOLET <theophilepicolet@hotmail.fr>
2025-04-26 17:58:32 +0200

    fix general

--- client/src/components/SignupForm.tsx
+++ client/src/components/SignupForm.tsx
@@ -199,7 +199,7 @@ export default function SignupForm() {
             </button>
             <ToastContainer
               position="top-right"
-              autoClose={5000}
+              autoClose={3000}
               hideProgressBar={false}
               newestOnTop={false}
               closeOnClick
